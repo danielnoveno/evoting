40643d60b68f51d782626debcbb685aaf76a38c3
danielnoveno <220711663@students.uajy.ac.id>
2026-05-28 19:19:58 +0700

    feat: add Content-Security-Policy header to next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "font-src 'self' *.vercel.com *.gstatic.com vercel.live k2mkucxia43oc7fa.public.blob.vercel-storage.com;",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
