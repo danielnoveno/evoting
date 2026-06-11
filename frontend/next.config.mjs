f37eb15619a560352cac3d1fc5759e9114f06abc
danielnoveno <220711663@students.uajy.ac.id>
2026-06-11 10:48:28 +0700

    chore: add Cross-Origin-Opener-Policy header to next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
         source: '/(.*)',
         headers: [
           {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+          {
             key: 'Content-Security-Policy',
             value: "font-src 'self' data: *.vercel.com *.gstatic.com vercel.live k2mkucxia43oc7fa.public.blob.vercel-storage.com;",
           },
